d108cefebfc982b3d460390540c9e067f30e7143
sogaiu <983021772@users.noreply.github.com>
2023-04-18 21:27:32 +0900

    Fill in syntax table some more

--- janet-ts-mode.el
+++ janet-ts-mode.el
@@ -104,6 +104,19 @@
     (modify-syntax-entry ?# "<" table)
     (modify-syntax-entry ?\n ">" table)
     ;;
+    (modify-syntax-entry ?! "_" table)
+    (modify-syntax-entry ?. "_" table)
+    (modify-syntax-entry ?: "_" table)
+    (modify-syntax-entry ?? "_" table)
+    (modify-syntax-entry ?^ "_" table)
+    ;;
+    (modify-syntax-entry ?| "'" table)
+    (modify-syntax-entry ?\; "'" table)
+    (modify-syntax-entry ?' "'" table)
+    (modify-syntax-entry ?~ "'" table)
+    (modify-syntax-entry ?, "'" table)
+    (modify-syntax-entry ?@ "'" table)
+    ;;
     table)
   ;;
   "Syntax table for `janet-ts-mode'.")
